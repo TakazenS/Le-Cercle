0f84527ce443807189a120f69c00c3b4b50cfc28
Loris <moine.loris420@gmail.com>
2026-07-16 23:45:02 +0200

    add: session token generation fn, some refractor on constants var

--- crates/server/src/auth.rs
+++ crates/server/src/auth.rs
@@ -6,15 +6,26 @@ use argon2::{
 };
 
 /*======= Constants =======*/
-const ALPHABET: &[u8] = b"ABCDEFGHIJKLMNPQRSTUVWXYZ123456789";
+const ACCESS_CODE_ALPHABET: &[u8] = b"ABCDEFGHIJKLMNPQRSTUVWXYZ123456789";
+const SESSION_TOKEN_ALPHABET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
 /*======= Functions =======*/
 pub fn generate_access_code() -> String {
     let mut rng = rand::rng();
     (0..8)
         .map(|_| {
-            let i = rng.random_range(0..ALPHABET.len());
-            ALPHABET[i] as char
+            let i = rng.random_range(0..ACCESS_CODE_ALPHABET.len());
+            ACCESS_CODE_ALPHABET[i] as char
+        })
+        .collect()
+}
+
+pub fn generate_session_token() -> String {
+    let mut rng = rand::rng();
+    (0..32)
+        .map(|_| {
+            let i = rng.random_range(0..SESSION_TOKEN_ALPHABET.len());
+            SESSION_TOKEN_ALPHABET[i] as char
         })
         .collect()
 }
