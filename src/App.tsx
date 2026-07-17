4df40fb737e2644bf3bfd32e13e38a2974479e03
Loris <moine.loris420@gmail.com>
2026-07-17 16:09:49 +0200

    add: check if a server is configured

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,41 @@
 import "./App.css";
 import { useAuth } from "./Auth/auth.tsx";
 import { AuthScreen } from "./Auth/AuthScreen.tsx";
+import { ServerLink } from "./Auth/ServerLink.tsx";
+import { FiServer } from "react-icons/fi";
+import { getUrl } from "./lib.ts";
+import { useState } from "react";
 
 function App() {
     const { isAuthenticated, logout } = useAuth();
-    if (!isAuthenticated) return <AuthScreen />;
+    const [showServerLink, setShowServerLink] = useState<boolean>(() => getUrl() === null);
+
+    if (!isAuthenticated) {
+        return (
+            <main className="mainAuth">
+                {showServerLink && (
+                    <ServerLink onClose={() => setShowServerLink(false)} />
+                )}
+                <AuthScreen />
+                <div className="toolbar">
+                    <button
+                        className="selectServer"
+                        onClick={() => {
+                            if (!showServerLink) {
+                                setShowServerLink(true)
+                            } else {
+                                setShowServerLink(false)
+                            }
+                        }}
+                    >
+                        <FiServer size={35}/>
+                    </button>
+                </div>
+            </main>
+        )
+    };
   return (
-    <main className="container">
+    <main className="mainApp">
         <p>Connecté ! (bientôt le reste)</p>
         <button onClick={logout}>Se deconnecter</button>
     </main>
