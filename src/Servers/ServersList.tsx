538196380e6a4a08c0d40955a35ffe159ad04fa3
Moine Loris <moine.loris420@gmail.com>
2026-07-18 15:31:19 +0200

    add: servers list component structure

--- src/Servers/ServersList.tsx
+++ src/Servers/ServersList.tsx
@@ -1,15 +1,47 @@
+import styles from "./ServersList.module.css";
 import { useServers } from "./ServersProvider.tsx";
 
-export function ServersList() {
-    const { servers } = useServers();
+interface Props {
+    onClose: () => void;
+}
+
+export function ServersList(props: Props) {
+    const { onClose } = props;
+    const { servers, selectServer, selectedId } = useServers();
 
     return (
-        <ul>
-            {servers.map(s => (
-                <li key={s.id}>
-                    {s.name} — {s.url}
-                </li>
-            ))}
-        </ul>
+        <section
+            className={styles.listSection}
+             onClick={(e) => {
+                 if (e.target === e.currentTarget) onClose();
+             }}
+        >
+            <div className={styles.abstract}>
+                <h1>Le Cercle</h1>
+                <p>Select a server to join.</p>
+            </div>
+            <div className={styles.card}>
+                <h3>Select a server</h3>
+                {servers.length !== 0 ? (
+                    <div className={styles.list}>
+                        {servers.map(s => (
+                            <button
+                                key={s.id}
+                                className={`${styles.item} ${s.id === selectedId ? styles.selected : styles.notSelected}`}
+                                onClick={() => {
+                                    selectServer(s.id);
+                                    onClose();
+                                }}
+                            >
+                                <p>{s.name}</p>
+                                <p>{s.url}</p>
+                            </button>
+                        ))}
+                    </div>
+                ) : (
+                    <p style={{color: "red"}}>No server found !</p>
+                )}
+            </div>
+        </section>
     );
 }
