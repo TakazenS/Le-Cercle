fe6de8bc2dd54c6ca2700e5a4803afa3efee9bca
Moine Loris <moine.loris420@gmail.com>
2026-07-18 02:16:16 +0200

    add: servers provider for reactive list

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthProvider } from "./Auth/auth.tsx";
+import { ServersProvider } from "./Servers/ServersProvider.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-      <AuthProvider>
-          <App />
-      </AuthProvider>
+      <ServersProvider>
+          <AuthProvider>
+              <App />
+          </AuthProvider>
+      </ServersProvider>
   </React.StrictMode>,
 );
