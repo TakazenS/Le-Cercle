3f4a1b8ea770c2b46fda208ab3d2c2b8048708be
Loris <moine.loris420@gmail.com>
2026-07-19 23:01:26 +0200

    refactor for trash modal, better structure

--- src/Servers/ServersList.tsx
+++ src/Servers/ServersList.tsx
@@ -1,9 +1,11 @@
 import styles from "./ServersList.module.css";
+import { Server } from "../models.ts";
 import { FiEdit3 } from "react-icons/fi";
 import { FaRegTrashCan } from "react-icons/fa6";
 import { useServers } from "./ServersProvider.tsx";
 import { useState } from "react";
-import { AddServers } from "./AddServers.tsx";
+import { ManageServers } from "./ManageServers.tsx";
+import { TrashModal } from "./TrashModal.tsx";
 
 interface Props {
     onClose: () => void;
@@ -11,85 +13,89 @@ interface Props {
 
 export function ServersList(props: Props) {
     const { onClose } = props;
-    const { servers, selectServer, selectedId } = useServers();
+    const { servers, selectedId, selectServer } = useServers();
     const [showEditServer, setShowEditServer] = useState<boolean>(false);
-    const [serverId, setServerId] = useState<string>("");
+    const [server, setServer] = useState<Server>();
+    const [isTrash, setIsTrash] = useState<boolean>(false);
 
     return showEditServer ? (
             <>
-                <AddServers onClose={() => onClose()} editServer={servers.find(s => s.id === serverId)}/>
+                <ManageServers onClose={() => setShowEditServer(false)} editServer={servers.find(s => s.id === server?.id)} />
             </>
         ) : (
-            <section
-                className={styles.listSection}
-                 onClick={(e) => {
-                     if (e.target === e.currentTarget) onClose();
-                 }}
-            >
-                <div className={styles.abstract}>
-                    <h1>Le Cercle</h1>
-                    <p>Select a server to join.</p>
-                </div>
-                <div className={styles.card}>
-                    <h3>Select a server</h3>
-                    {servers.length !== 0 ? (
-                        <div className={styles.list}>
-                            {servers.map(s => (
-                                <button
-                                    key={s.id}
-                                    className={`${styles.item} ${s.id === selectedId ? styles.selected : styles.notSelected}`}
-                                    onClick={(e) => {
-                                        if (e.target === e.currentTarget) {
+            <>
+                {isTrash && (
+                    <TrashModal onClose={() => setIsTrash(false)} server={server} />
+                )}
+                <section
+                    className={styles.listSection}
+                     onClick={(e) => {
+                         if (e.target === e.currentTarget) {
+                             e.stopPropagation();
+                             onClose();
+                         }
+                     }}
+                >
+                    <div className={styles.abstract}>
+                        <h1>Le Cercle</h1>
+                        <p>Select a server to join.</p>
+                    </div>
+                    <div className={styles.card}>
+                        <h3>Select a server</h3>
+                        {servers.length !== 0 ? (
+                            <div className={styles.list}>
+                                {servers.map(s => (
+                                    <div
+                                        key={s.id}
+                                        className={`${styles.item} ${s.id === selectedId ? styles.selected : styles.notSelected}`}
+                                        onClick={() => {
                                             selectServer(s.id);
                                             onClose();
-                                        }
-                                    }}
-                                >
-                                    <div>
-                                        <p
-                                            onClick={(e) => {
-                                                if (e.target === e.currentTarget) {
-                                                    selectServer(s.id);
-                                                    onClose();
-                                                }
-                                            }}
-                                        >
-                                            {s.name}
-                                        </p>
-                                        <p
-                                            onClick={(e) => {
-                                                if (e.target === e.currentTarget) {
-                                                    selectServer(s.id);
-                                                    onClose();
-                                                }
-                                            }}
-                                        >
-                                            {s.url}
-                                        </p>
+                                        }}
+                                        role="button"
+                                        tabIndex={0}
+                                        onKeyDown={(e) => {
+                                            if (e.key === "Enter" || e.key === " ") {
+                                                e.preventDefault();
+                                                selectServer(s.id);
+                                                onClose();
+                                            }
+                                        }}
+                                    >
+                                        <div>
+                                            <p>{s.name}</p>
+                                            <p>{s.url}</p>
+                                        </div>
+                                        <div className={styles.iconBox}>
+                                            <button
+                                                className={styles.editBtn}
+                                                onClick={(e) => {
+                                                    e.stopPropagation();
+                                                    setShowEditServer(true);
+                                                    setServer(s);
+                                                }}
+                                            >
+                                                <FiEdit3 size={20}/>
+                                            </button>
+                                            <button
+                                                className={styles.deleteBtn}
+                                                onClick={(e) => {
+                                                    e.stopPropagation();
+                                                    setIsTrash(true);
+                                                    setServer(s)
+                                                }}
+                                            >
+                                                <FaRegTrashCan size={20}/>
+                                            </button>
+                                        </div>
                                     </div>
-                                    <div className={styles.iconBox}>
-                                        <button
-                                            className={styles.editBtn}
-                                            onClick={() => {
-                                                setShowEditServer(true);
-                                                setServerId(s.id);
-                                            }}
-                                        >
-                                            <FiEdit3 size={20}/>
-                                        </button>
-                                        <button
-                                            className={styles.deleteBtn}
-                                        >
-                                            <FaRegTrashCan size={20}/>
-                                        </button>
-                                    </div>
-                                </button>
-                            ))}
-                        </div>
-                    ) : (
-                        <p style={{color: "red"}}>No server found !</p>
-                    )}
-                </div>
-            </section>
+                                ))}
+                            </div>
+                        ) : (
+                            <p style={{color: "red"}}>No server found !</p>
+                        )}
+                    </div>
+                </section>
+            </>
     );
 }
