9b93878c304fa9bf356a4c5a6b4c26e9a4106962
Loris <moine.loris420@gmail.com>
2026-07-21 21:40:46 +0200

    add: loading screen on app launch

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import { AuthProvider } from "./Auth/AuthProvider.tsx";
-import { ServersProvider } from "./Servers/ServersProvider.tsx";
+import { getCurrentWindow } from "@tauri-apps/api/window";
 import { ThemeProvider } from "./Theme/ThemeProvider.tsx";
+import { ServersProvider } from "./Servers/ServersProvider.tsx";
+import { AuthProvider } from "./Auth/AuthProvider.tsx";
+import App from "./App";
+
+getCurrentWindow().show();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
